2d1de69e6829c1967188a2b7f98dbec534d4df4f
izzylp <luispaulomr@gmail.com>
2021-08-02 03:03:13 -0300

    Updated Assignment 6

--- assignment_6/test_4.py
+++ assignment_6/test_4.py
@@ -26,39 +26,12 @@ if N - p * q == 0:
 	
 	d = gmpy2.powmod(e, -1, phi)
 	
-	#print(hex(gmpy2.powmod(y, d, N)))
-	#print(pow(y, d))
-	
-	#result = bytearray.fromhex(hex(gmpy2.powmod(y, d, N)))
-	
-	#print(result)
-	
-	#some_int = gmpy2.powmod(y, d, N)
-	#some_int = gmpy2.int()
-	#some_bytes = some_int.to_bytes(32, sys.byteorder)
-	#my_bytearray = bytearray(some_bytes)
-	
 	x = gmpy2.powmod(y, d, N)
 	x_hex = hex(x)
 	x_hex = x_hex[2 : len(x_hex)]
-	print(x_hex)
 	x_hex = "0" + x_hex
-	print(x_hex)
-	
-	#print(x_hex[253])
 	
 	CT_bytearray = bytearray.fromhex(x_hex)
 	
-	print(CT_bytearray)
-	
-	for byte in CT_bytearray:
-	
-		#print(byte)
-		if byte == 0:
-		
-			print("LUL")
-	
-	#print(binascii.unhexlify(hex(x)[2:]))
-	
-	#print(CT_bytearray)
+	print(CT_bytearray)W
 	
